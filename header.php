11dc0ce16880177665be9497de3bfa9f41c1c0fd
CestVraimentTropDrole <149939786+CestVraimentTropDrole@users.noreply.github.com>
2025-01-13 13:34:10 +0100

    feat: header links

--- header.php
+++ header.php
@@ -10,9 +10,20 @@
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <?php wp_head(); ?>
   </head>
-  <body>
-    <header>
-      <div class="menu-container">
+  <body class="flex flex-row">
+    <header class="py-8 px-7 h-dvh border-2 border-black border-solid">
+      <div class="menu-container flex flex-col items-center">
+        <a href="<?php echo home_url(); ?>">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" alt="Logo" class="w-28 h-32">
+        </a>
         <?php wp_nav_menu(array('menu' => 'primary_menu')); ?>
+        <div class="menu-link-container w-full">
+          <ul class="flex flex-row">
+            <li class="menu-item-object-page"><a>In</a></li>
+            <li class="menu-item-object-page"><a>Fa</a></li>
+            <li class="menu-item-object-page"><a>LI</a></li>
+            <li class="menu-item-object-page"><a>LT</a></li>
+          </ul>
+        </div>
       </div>
     </header>
