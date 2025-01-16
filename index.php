dc28a5b3f8c27a2b6b49775407031f7fe225e25b
CestVraimentTropDrole <149939786+CestVraimentTropDrole@users.noreply.github.com>
2025-01-16 10:56:28 +0100

    feat: article page start

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
-    <?php get_header(); ?>
-    
-    <div>
-        <section>
-            Page normale
-        </section>
-    </div>
+<?php
+    /*
+        Template Name: Single
+    */    
+?>
 
-    <?php get_footer(); ?>
+<?php get_header(); ?>
+    
+    <div class="w-full h-fit flex flex-col gap-16">
+        <section id="article" class="flex flex-col items-center gap-14 py-20">
+            <p>Article</p>
+        </section>
+
+<?php get_footer(); ?>
